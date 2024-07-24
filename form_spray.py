dd88570dfa1bcacf183667d12b9e41391800c57d
Lazuardis <lazuardy.almuzaki@gmail.com>
2024-07-24 11:20:24 +0700

    first commit

---
+++ form_spray.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import datetime
+
+
+
+# Streamlit app title
+st.title("Data Entry Dashboard")
+
+# Load data
+spray_data = pd.read_csv('form_spray.csv')
+blok_katalog = pd.read_csv('lokasi_katalog.csv', header=None)
+blok_katalog_options = blok_katalog.values.flatten().tolist()
+
+spray_katalog = pd.read_csv('spray_katalog.csv', header=None)
+spray_katalog.columns = ['Material', 'Unit']
+spray_katalog['Takaran'] = 0
+
+# reorder columns name of spray_katalog into material, takaran, unit 
+spray_katalog = spray_katalog[['Material', 'Takaran', 'Unit']]
+
+
+# User input fields
+st.header("Enter your data:")
+date = st.date_input("Tanggal Pelaksanaan")
+dropdown_selection = st.selectbox("Lokasi", blok_katalog_options)
+tangki = st.number_input("Jumlah Tangki", min_value=0, max_value=120, step=1)
+data_editor = st.data_editor(spray_katalog)
+
+# Create new_data DataFrame with the same columns as spray_data
+new_data = pd.DataFrame(columns=spray_data.columns)
+
+# Populate new_data with the values from data_editor and the input fields
+for index, row in data_editor.iterrows():
+    material = row['Material']
+    takaran = row['Takaran']
+    unit =  row['Unit']
+    new_row = {
+        'tanggal_pengisian': datetime.date.today(),
+        'tanggal_pelaksanaan': date,
+        'blok': dropdown_selection,
+        'jumlah_tangki': tangki,
+        'material': material,
+        'takaran': takaran,
+        'unit': unit
+    }
+    new_data = new_data.append(new_row, ignore_index=True)
+
+# drop rows with zero takaran
+new_data = new_data[new_data['takaran'] != 0]
+
+# Show the new_data DataFrame
+# st.write("New Data to be Saved:")
+# st.dataframe(new_data)
+
+# Save the new data to CSV (append mode)
+if st.button('Save'):
+    spray_data = spray_data.append(new_data, ignore_index=True)
+    spray_data.to_csv('form_spray.csv', index=False)
+    st.success("Data Sukses Tersimpan!")
+
+st.markdown('''
+---
+Created by [Your Name](https://github.com/your-github)
+''')
