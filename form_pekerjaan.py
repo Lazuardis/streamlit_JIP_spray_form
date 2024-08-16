3f1b6e5571d74342945960b8aa25eee25fd0233f
Lazuardis <lazuardy.almuzaki@gmail.com>
2024-08-16 21:38:56 +0700

    feat: Add lazy loading for images in form_pekerjaan.py

--- form_pekerjaan.py
+++ form_pekerjaan.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from streamlit_gsheets import GSheetsConnection
+from streamlit.dataframe_util import convert_anything_to_pandas_df
 
 # Streamlit app title
 st.title("Form Pekerjaan")
