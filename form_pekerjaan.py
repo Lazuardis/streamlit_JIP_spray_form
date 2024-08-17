cd14c56cf1925c77f6156b7cb76902812b7fd360
Lazuardis <lazuardy.almuzaki@gmail.com>
2024-08-17 14:39:58 +0700

    feat: Add form for Jojoh Pupuk and save data to pupuk_jojoh worksheet

--- form_pekerjaan.py
+++ form_pekerjaan.py
@@ -10,7 +10,7 @@ st.title("Form Pekerjaan")
 
 # Select box for choosing the type of work
 job_type = st.selectbox("Jenis Pekerjaan",
-                        options=["", "Kocor Pupuk", "Spray"])
+                        options=["", "Kocor Pupuk", "Jojoh Pupuk" ,"Spray",])
 
 url = "https://docs.google.com/spreadsheets/d/1xoFAFQjNUDNohoAhWzkwEyOoQ4d7_gHTtYAEXvNBq-c/edit?gid=0#gid=0"
 
@@ -145,6 +145,73 @@ elif job_type == "Spray":
             updated_data = pd.concat([existing_data, new_data],
                                      ignore_index=True)
             conn.update(worksheet='spray', data=updated_data)
+            st.success("Data berhasil disimpan!")
+
+if job_type == "Jojoh Pupuk":
+    st.header("Form Pemupukan Jojoh")
+
+    conn = st.connection('gsheets', type=GSheetsConnection)
+    # st.write(conn)
+    existing_data = conn.read(spreadsheet=url,worksheet='pupuk_jojoh', usecols=list(range(7)), ttl=5)
+
+    # Load data
+    blok_katalog = pd.read_csv('lokasi_katalog.csv', header=None)
+    blok_katalog_options = blok_katalog.values.flatten().tolist()
+
+    pupuk_katalog = pd.read_csv('pupuk_katalog.csv', header=None)
+    pupuk_katalog.columns = ['Material', 'Unit']
+    pupuk_katalog['Takaran'] = 0
+
+    # Reorder columns
+    pupuk_katalog = pupuk_katalog[['Material', 'Takaran', 'Unit']]
+
+    # User input fields
+    date = st.date_input("Tanggal Pelaksanaan")
+    dropdown_selection = st.selectbox("Lokasi",
+                                      options=blok_katalog_options,
+                                      index=None)
+    pohon = st.number_input("Jumlah Pohon",
+                             min_value=0,
+                             max_value=120,
+                             step=1)
+
+    pupuk_katalog_reset = pupuk_katalog.reset_index(drop=True)
+    data_editor = st.data_editor(pupuk_katalog_reset)
+
+    # Create new data DataFrame
+    new_data = pd.DataFrame(columns=existing_data.columns)
+    for index, row in data_editor.iterrows():
+        material = row['Material']
+        takaran = row['Takaran']
+        unit = row['Unit']
+        new_row = {
+            'tanggal_pengisian': datetime.date.today(),
+            'tanggal_pelaksanaan': date,
+            'blok': dropdown_selection,
+            'jumlah_pohon': pohon,
+            'material': material,
+            'takaran': takaran,
+            'unit': unit
+        }
+        new_data = pd.concat([new_data, pd.DataFrame([new_row])],
+                             ignore_index=True)
+
+    # Drop rows with zero takaran
+    new_data = new_data[new_data['takaran'] != 0]
+
+    save_button = st.button('Save')
+
+    if save_button:
+        if not date or not dropdown_selection:
+            st.warning("Pengisian data belum lengkap!")
+            st.stop()
+        elif tangki == 0:
+            st.warning("Jumlah tangki tidak boleh nol!")
+            st.stop()
+        else:
+            updated_data = pd.concat([existing_data, new_data],
+                                     ignore_index=True)
+            conn.update(worksheet='pupuk_kocor', data=updated_data)
             st.success("Data berhasil disimpan!")
 
 st.markdown('''
